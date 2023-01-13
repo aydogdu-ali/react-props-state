a33442072f19a6646fd04b0ae696af27564eda97
Ali <ali.ozgur@example.com>
2023-01-13 16:54:50 +0300

    data js added

---
+++ src/components/data.js
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    id: 1,
+    name: "Hatice Yeter",
+    birth: "28-01-1985",
+    age:38,
+
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    birth: "19-01-1990",
+    age: 33,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Adem Little",
+    birth: "15-01-1991",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Yusuf Walsh",
+    birth: "31-01-1987",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lale Gardner",
+    birth: "22-01-1999",
+    age: 24,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
